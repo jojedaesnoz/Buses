2d29bc86810a5def107a6fbd57c84ef7c5b7bd21
jojedaesnoz <jojedaesnoz@gmail.com>
2019-03-06 13:31:28 +0100

    Final

--- DatosCompartidos/src/jojeda/Constantes.java
+++ DatosCompartidos/src/jojeda/Constantes.java
@@ -1,10 +1,23 @@
 package jojeda;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Constantes {
 
     public static final int MAX_BUSES = 3;
-    public static String SERVER_IP = "192.168.255.86";
+    public static final BufferedReader TECLADO = new BufferedReader(new InputStreamReader(System.in));
+    public static String SERVER_IP;
+
+    static {
+        try {
+            SERVER_IP = TECLADO.readLine();
+        } catch (IOException e) {
+            SERVER_IP = "192.168.255.86";
+        }
+    }
 
     public final static int MARQUESINAS_PORT = 5555;
     public final static int BUSES_PORT = 5556;
